1ae206f475d19eff282128490651c3165b3d109c
Chris Segrove <myantipodeanpenpal@gmail.com>
2022-04-19 22:01:33 +0100

    Add documentation

---
+++ LexAssert.Tests/Demo/LexAssertDemos.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+
+namespace LexAssert.Tests.Demo
+{
+    public class LexAssertDemos
+    {
+        internal class MyTestClass
+        {
+            string PropA => "foo";
+            int PropB => 63;
+        }
+
+        [Fact]
+        public void JsonEqual_Demo1()
+        {
+            var actual = new MyTestClass();
+            var expected = new MyTestClass();
+
+            LexAssert.JsonEqual(expected, actual); // Passes
+        }
+
+        internal class AnotherTestClass
+        {
+            public string PropA { get; set; }
+            public int PropB { get; set; }
+            public System.Guid PropC { get; set; }
+        }
+
+        [Fact]
+        public void MembersEqual_Demo1()
+        {
+            var actual = new AnotherTestClass
+            {
+                PropA = "foo",
+                PropB = 63,
+                PropC = System.Guid.NewGuid()
+            };
+
+            var expected = new AnotherTestClass
+            {
+                PropA = "foo",
+                PropB = 63,
+                PropC = System.Guid.NewGuid()
+            };
+
+            LexAssert.MembersEqual(expected, actual,
+                c => c.PropA,
+                c => c.PropB);  // Passes, because PropC is not compared.
+        }
+
+        public void MembersEqual_Demo2()
+        {
+            var actual = "foo";
+            var expected = "bar";
+
+            LexAssert.MembersEqual(expected, actual,
+                c => c.ToLowerInvariant().Contains("z")); // Passes, because false == false.
+        }
+    }
+}
